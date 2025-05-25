8a9db630510a07fb4ee85cf7e59fb2f1c26ba8b9
naitik690 <naitik.srg@gmail.com>
2025-05-25 21:28:56 +0530

    Update skill.jsx

--- skill.jsx
+++ skill.jsx
@@ -10,9 +10,9 @@ function Skills() {
        <h2 className='yellow'>✨Technical Skills</h2>
        <div className="li">
 <li>Frontend Technologies: HTML5, CSS3, JavaScript (ES6+), ReactJS</li>
-<li>Styling: CSS Flexbox, Grid, Responsive Design, Bootstrap (agar aata ho)</li>
+<li>Styling: CSS Flexbox, Grid, Responsive Design, Bootstrap </li>
 <li>JavaScript Frameworks & Libraries: ReactJS, JSX, React Router, State Management (useState, useEffect, etc.)</li>
-<li>Version Control: Git, GitHub (agar aata ho)</li>
+<li>Version Control: Git, GitHub </li>
 </div>
 </div>
     </>
